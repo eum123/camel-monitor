b30177633aaf902d704666d6e5163212308f221d
manjineum <manjineum@wemakeprice.com>
2018-12-12 10:07:25 +0900

    라우터 파일 선택 이벤트 추가

--- app/info/routeInfo.js
+++ app/info/routeInfo.js
@@ -12,59 +12,54 @@
 //     }]);
 
 //app create by main.js
-app.controller('routeFilesCtrl', ['$scope', function ($scope)  {
+app.controller('routeFilesCtrl', ['$scope', function ($scope) {
 
-        var data = JSON.parse('{\n' +
-            '  "result_code": "OK",\n' +
-            '  "result_message": "정상",\n' +
-            '  "result": [\n' +
-            '    "/work/manjin/project/camel-launcher/src/test/resources/conf/route/process.xml",\n' +
-            '    "/work/manjin/project/camel-launcher/src/test/resources/conf/route/process1.xml"\n' +
-            '  ]\n' +
-            '}');
-        $scope.routeFiles = data.result;
+    var data = JSON.parse('{\n' +
+        '  "result_code": "OK",\n' +
+        '  "result_message": "정상",\n' +
+        '  "result": [\n' +
+        '    "/work/manjin/project/camel-launcher/src/test/resources/conf/route/process.xml",\n' +
+        '    "/work/manjin/project/camel-launcher/src/test/resources/conf/route/process1.xml"\n' +
+        '  ]\n' +
+        '}');
+    $scope.routeFiles = data.result;
 
-        // $scope.click = function(id) {
-        //     console.log(id);
-        //     $("#nav-link active").class('nav-link');
-        //     console.log($("#nav-link active"));
-        // }
+    // $scope.click = function(id) {
+    //     console.log(id);
+    //     $("#nav-link active").class('nav-link');
+    //     console.log($("#nav-link active"));
+    // }
 
-    }]);
+}]);
 
 app.controller('RouteInfoCtrl', [function () {
 
-    }]);
+}]);
 
-app.filter('getFileName', function(){
-   return function (input) {
-       if(input.indexOf('/') >= 0 ) {
-           return input.substring(input.lastIndexOf('/') + 1);
-       } else {
-           return input;
-       }
 
-   }
-});
-
+/**
+ * 라우터 파일 선택시(nav-link) 이벤트
+ */
 $('div').on('click', 'a', function (e) {
 
-    $(".nav-link").each(function() {
-      //  $(this).removeClass();
-        console.log($(this));
-    });
+    $('#v-pills-tab a').each(function () {
+        console.log($(this).attr('class', 'nav-link'));
+    })
 
-    //console.log($('div').children('.nav-link active'));
-
-    //console.log(this.parentNode.childNodes);
-
-    //console.log(e);
-
-    // $('div').children('a').forEach(function(value, index, array1) {
-    //     value.tab('hide');
-    // });
-
-     //$(this).tab('show')
-     e.preventDefault()
+    e.preventDefault()
 });
 
+
+/////////////////////////////////////
+////  filter
+////////////////////////////////////
+app.filter('getFileName', function () {
+    return function (input) {
+        if (input.indexOf('/') >= 0) {
+            return input.substring(input.lastIndexOf('/') + 1);
+        } else {
+            return input;
+        }
+
+    }
+});
